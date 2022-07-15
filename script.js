bb902168678b033cde9628c6080dae45a74a0e71
Brian Denmark <testingarea479@gmail.com>
2022-07-14 20:59:40 -0700

    stopping foreach functionality for now, not sure how to fix undefined error

--- script.js
+++ script.js
@@ -1,33 +1,70 @@
-console.log("page loaded...");
-document.getElementById("myVideo").muted = true;
-let x = document.getElementById("myVideo");
+// Query Selectors/getElementsBy...
 
+document.getElementById("myVideo").muted = true;
+let mainVideo = document.getElementById("myVideo");
+let logBtn = document.getElementById("l-btn");
+let subBtn = document.getElementById("s-btn");
+let upcomingVideo = document.querySelectorAll(".vid-s");
+
+console.log(upcomingVideo.length);
+
+// Event Listeners
+mainVideo.addEventListener("mouseover", ()=> {
+    playVideo();
+});
+
+mainVideo.addEventListener('mouseleave', ()=> {
+    pauseVideo();
+});
+
+logBtn.addEventListener("click", ()=> {
+    logStatus();
+});
+
+subBtn.addEventListener("click", ()=> {
+    subStatus();
+});
+
+upcomingVideo.forEach(element => element.addEventListener("click", ()=> {
+    addHover();
+}));
+
+// upcomingVideo.addEventListener("mouseleave", ()=> {
+//     removeHover();
+// });
+
+// Functions
 function playVideo() {
     console.log("mouse over video...");
-    x.play();
+    mainVideo.play();
 }
 
 function pauseVideo() {
     console.log("mouse left video...");
-    x.pause();
+    mainVideo.pause();
 }
 
-function logStatus(element) {
-    if (element.textContent == "Login") {
-        element.textContent = "Logout";
-    } else if (element.textContent == "Logout") {
-        element.textContent = "Login";
+function logStatus() {
+    if (logBtn.textContent == "Login") {
+        logBtn.textContent = "Logout";
+    } else if (logBtn.textContent == "Logout") {
+        logBtn.textContent = "Login";
     }
 }
 
-function subStatus(element) {
-    if (element.textContent == "Subscribe") {
-        element.textContent = "Unsubscribe";
-    } else if (element.textContent == "Unsubscribe") {
-        element.textContent = "Subscribe";
+function subStatus() {
+    if (subBtn.textContent == "Subscribe") {
+        subBtn.textContent = "Unsubscribe";
+    } else if (subBtn.textContent == "Unsubscribe") {
+        subBtn.textContent = "Subscribe";
     }
 }
 
-function hoverVidPreview(element) {
-    element.classList.add("video-small-hover");
+function addHover() {
+    console.log("starting addHover()...")
+    upcomingVideo.classList.add("video-small-hover");
+}
+
+function removeHover() {
+    // upcomingVideo.classList.remove("video-small-hover");
 }
